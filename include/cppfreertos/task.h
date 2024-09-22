5e208362d377f53244219dae9700c8fc03ce9f70
Markus Pielmeier <markuspi@users.noreply.github.com>
2024-09-22 13:41:08 +0200

    Task notifications

--- include/cppfreertos/task.h
+++ include/cppfreertos/task.h
@@ -3,6 +3,8 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#include <utility>
+
 namespace cppfreertos {
 
 using CbFunction = void (*)();
@@ -10,14 +12,11 @@ using CbFunction = void (*)();
 class BaseTask {
    protected:
     TaskHandle_t handle_{nullptr};
-    CbFunction callback_{nullptr};
 
-    static void Dispatch(void* param);
+    BaseTask() = default;
 
    public:
-    BaseTask(CbFunction callback);
     ~BaseTask() = default;
-
     BaseTask(const BaseTask&) = delete;
     BaseTask& operator=(const BaseTask&) = delete;
     BaseTask(BaseTask&&) = delete;
@@ -25,7 +24,12 @@ class BaseTask {
 
     TaskHandle_t GetHandle() const;
 
-    void NotifyGiveFromISR(BaseType_t& higher_prio_task_woken);
+    void NotifyGive(UBaseType_t index_to_notify = 0);
+
+    void NotifyGiveFromISR(BaseType_t& higher_prio_task_woken, UBaseType_t index_to_notify = 0);
+
+    static uint32_t NotifyTake(bool clear_count_on_exit, TickType_t ticks_to_wait,
+                               UBaseType_t index_to_wait = 0);
 
     void Resume();
 
@@ -36,24 +40,35 @@ class BaseTask {
     static void Delay(TickType_t delay_ms);
 };
 
+class TaskJob {
+    CbFunction callback_;
+
+   public:
+    TaskJob(CbFunction callback);
+
+    static void Dispatch(void* param);
+};
+
 template <size_t TStackSize>
 class StaticTask : public BaseTask {
     StackType_t stack_storage_[TStackSize];
     StaticTask_t task_storage_{};
+    TaskJob job_;
 
    public:
-    using BaseTask::BaseTask;
+    StaticTask(CbFunction callback) : job_(std::move(callback)) {}
 
     void Init(const char* name, UBaseType_t priority = 1, BaseType_t core_id = tskNO_AFFINITY) {
-        handle_ = xTaskCreateStaticPinnedToCore(BaseTask::Dispatch, name, TStackSize, this,
+        handle_ = xTaskCreateStaticPinnedToCore(TaskJob::Dispatch, name, TStackSize, &job_,
                                                 priority, stack_storage_, &task_storage_, core_id);
     }
 };
 
 class Task : public BaseTask {
-   public:
-    using BaseTask::BaseTask;
+    TaskJob job_;
 
+   public:
+    Task(CbFunction callback);
     ~Task();
 
     bool Init(const char* name, uint32_t stack_size, UBaseType_t priority = 1,
--- src/task.cpp
+++ src/task.cpp
@@ -5,19 +5,22 @@
 
 namespace cppfreertos {
 
-void BaseTask::Dispatch(void* param) {
-    static_cast<BaseTask*>(param)->callback_();
-    abort();
-}
-
-BaseTask::BaseTask(CbFunction callback) : callback_(callback) {}
-
 TaskHandle_t BaseTask::GetHandle() const {
     return handle_;
 }
 
-void BaseTask::NotifyGiveFromISR(BaseType_t& higher_prio_task_woken) {
-    vTaskNotifyGiveFromISR(handle_, &higher_prio_task_woken);
+void BaseTask::NotifyGive(UBaseType_t index_to_notify) {
+    xTaskNotifyGiveIndexed(handle_, index_to_notify);
+}
+
+void BaseTask::NotifyGiveFromISR(BaseType_t& higher_prio_task_woken, UBaseType_t index_to_notify) {
+    vTaskNotifyGiveIndexedFromISR(handle_, index_to_notify, &higher_prio_task_woken);
+}
+
+uint32_t BaseTask::NotifyTake(bool clear_count_on_exit, TickType_t ticks_to_wait,
+                              UBaseType_t index_to_wait) {
+    return ulTaskNotifyTakeIndexed(index_to_wait, clear_count_on_exit ? pdTRUE : pdFALSE,
+                                   ticks_to_wait);
 }
 
 void BaseTask::Resume() {
@@ -36,6 +39,8 @@ void BaseTask::Delay(TickType_t delay_ms) {
     vTaskDelay(pdMS_TO_TICKS(delay_ms));
 }
 
+Task::Task(CbFunction callback) : job_(std::move(callback)) {}
+
 Task::~Task() {
     if (handle_ != nullptr) {
         vTaskDelete(handle_);
@@ -43,7 +48,7 @@ Task::~Task() {
 }
 
 bool Task::Init(const char* name, uint32_t stack_size, UBaseType_t priority, BaseType_t core_id) {
-    auto status = xTaskCreatePinnedToCore(BaseTask::Dispatch, name, stack_size, this, priority,
+    auto status = xTaskCreatePinnedToCore(TaskJob::Dispatch, name, stack_size, &job_, priority,
                                           &handle_, core_id);
     if (status == pdPASS) {
         return true;
@@ -51,5 +56,12 @@ bool Task::Init(const char* name, uint32_t stack_size, UBaseType_t priority, Bas
     handle_ = nullptr;
     return false;
 }
+
+void TaskJob::Dispatch(void* param) {
+    static_cast<TaskJob*>(param)->callback_();
+    abort();
+}
+
+TaskJob::TaskJob(CbFunction callback) : callback_(callback) {}
 
 }  // namespace cppfreertos
